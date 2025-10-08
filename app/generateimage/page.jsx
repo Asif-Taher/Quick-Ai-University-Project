59a9633bbfc9b227c8da808e34426b3914cf27d7
Asif Taher <mdasiftaher2020@gmail.com>
2025-10-08 16:59:21 +0600

    Initial commit

---
+++ app/generateimage/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h2>generate page</h2>
+    </div>
+  )
+}
+
+export default page
---
+++ components/GenerateImage/GenerateImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GenerateImage = () => {
+  return (
+    <div>
+      <h2>This is the generate image</h2>
+      <h1>image generate</h1>
+    </div>
+  )
+}
+
+export default GenerateImage
