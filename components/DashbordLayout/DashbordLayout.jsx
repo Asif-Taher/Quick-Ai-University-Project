cab0f8fbba88dce39be059a35f7514563ffc6810
Asif Taher <mdasiftaher2020@gmail.com>
2026-01-01 17:36:11 +0600

    add new line

--- components/DashbordLayout/DashbordLayout.jsx
+++ components/DashbordLayout/DashbordLayout.jsx
@@ -3,18 +3,20 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import logo from '@/public/assets/logo.svg'
 import { Menu, X } from 'lucide-react';
+import Outlet from './Outlet';
 
 const DashbordLayout = () => {
 
     const [sidebar, setsidebar] = useState(false);
   return (
     <div className='flex flex-col items-start justify-start h-screen'>
-     <nav className='flex justify-between border-b border-b-gray-200 border-solid w-full px-3 py-2'>
+     <nav className='flex justify-between items-center border-b border-b-gray-200 border-solid w-full px-3 py-2'>
         <Image src={logo} alt='logo' width={220} height={220}/>
         {
-            sidebar ? <X className='w-6 h-6 text-gray-300 sm:hidden'/> : <Menu className='h-6 w-6 text-gray-300 sm:hidden'/>
+            sidebar ? <X onClick={()=> setsidebar(false)} className='w-6 h-6 text-gray-300 sm:hidden'/> : <Menu onClick={()=> setsidebar(true)} className='h-6 w-6 text-gray-300 sm:hidden'/>
         }
      </nav>
+     <Outlet />
     </div>
   )
 }
---
+++ components/DashbordLayout/Outlet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Outlet = () => {
+  return (
+    <div>
+      <h2>Dashbord</h2>
+    </div>
+  )
+}
+
+export default Outlet
